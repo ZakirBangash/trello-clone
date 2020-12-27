4bd5c5c7ebc411178e6c5ef641ab33b0fad4328b
Abdurrahman-Alizada <63086457+Abdurrahman-Alizada@users.noreply.github.com>
2020-12-27 10:29:29 +0500

    card clikkable

--- src/Components/SubCard.js
+++ src/Components/SubCard.js
@@ -3,10 +3,15 @@ import "./styles/SubCard.css";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
-
+import Button from "@material-ui/core/Button"
 export default function SubCard({ title }) {
+  
+  const Test = ()=>{
+    console.log('clicked card')
+  }
+  
   return (
-    <Card className="subCard">
+<Card className="subCard" onClick={Test}>
       <CardContent>
         <div className="subCard__Content">
           <h6>{title}</h6>
